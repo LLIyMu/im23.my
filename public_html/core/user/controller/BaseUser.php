5f236ffb42b182a7732db392b84c0a85a59b28bf
LLIyMu <52199092+LLIyMu@users.noreply.github.com>
2024-04-26 09:57:56 +0500

    2

--- public_html/core/user/controller/BaseUser.php
+++ public_html/core/user/controller/BaseUser.php
@@ -22,7 +22,22 @@ abstract class BaseUser extends BaseController
 
     protected function outputData()
     {
-        
+
+        if (!$this->content){
+            $args = func_get_arg(0);
+            $vars = isset($args) ? : [];
+
+            //if (!$this->template) $this->template = ADMIN_TEMPLATE . 'show';
+
+            $this->content = $this->render($this->template, $vars);
+        }
+
+        $this->header = $this->render(TEMPLATE . 'include/header', $vars);
+
+        $this->footer = $this->render(TEMPLATE . 'include/footer', $vars);
+
+        return $this->render(TEMPLATE . 'layout/default');
+
     }
 
 }
--- public_html/core/user/controller/IndexController.php
+++ public_html/core/user/controller/IndexController.php
@@ -3,19 +3,12 @@
 namespace core\user\controller;
 
 use core\admin\model\Model;
-use core\base\controller\BaseController;
 
-class IndexController extends BaseController{
+class IndexController extends BaseUser {
 	
 	protected function inputData(){
 		
-		$this->init();
-
-		$header = $this->render(TEMPLATE . 'header');
-		$content = $this->render();
-		$footer = $this->render(TEMPLATE . 'footer');
 		
-		return $this->render(TEMPLATE . 'templater', compact('header', 'content', 'footer'));
  
 	}
 	
